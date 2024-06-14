fd27a5b04a6f5d3ebae6455137c557b58e73d043
Bernhard Schoeffel <bernhard.schoeffel@gmail.com>
2024-06-15 00:22:39 +0200

    Added Debug to the simple Pouring logic to try it

--- src/Game.svelte
+++ src/Game.svelte
@@ -1,16 +1,16 @@
 <script>  
-  import GameBoard from './GameBoard.svelte';  
-  import GameFrame from './GameFrame.svelte';  
+    import GameBoard from './GameBoard.svelte';  
+    import GameFrame from './GameFrame.svelte';  
     
-  // Anfangszustand des Spiels als JSON-Objekt  
-  let gameState = {  
-    bottles: [  
-      { id: 1, levels: [{ color: 'blue' }, { color: 'red' }, { color: 'yellow'}, { color: 'green'}, { color: 'purple'}]},  
-      { id: 2, levels: [{ color: 'yellow' }] },  
-    ]  
-  };  
+    // The intial Game State 
+    let gameState = {  
+        bottles: [  
+            { id: 1, levels: [{ color: 'yellow' }, { color: 'yellow' }, { color: 'yellow'}, { color: 'yellow'}, { color: 'yellow'}]},  
+            { id: 2, levels: [{ color: 'yellow' }] },  
+        ]  
+    };  
 </script>  
   
 <GameFrame>  
-  <GameBoard bind:gameState={gameState} />  
+    <GameBoard bind:gameState={gameState} />  
 </GameFrame>  
