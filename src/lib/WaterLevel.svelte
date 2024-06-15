ed4abbfd1c5ccaea1d3104f01f286b18559a8670
Bernhard Schoeffel <bernhard.schoeffel@gmail.com>
2024-06-15 10:27:31 +0200

    Added Animation to water transition

--- src/lib/WaterLevel.svelte
+++ src/lib/WaterLevel.svelte
@@ -1,8 +1,10 @@
 <script>  
-  export let color;  
+    import { slide } from "svelte/transition";
+
+    export let color;  
 </script>  
   
-<div class="water-level" style="background-color: {color};">
+<div class="water-level" style="background-color: {color};" in:slide={{ duration: 1000 }}>
 
 </div>  
   
@@ -10,7 +12,6 @@
     .water-level {  
         height: 20%;                        /* Every Waterlevel should Take up 20% of the overall height */  
         width: 100%;  
-        transition: background-color 0.3s;  
         box-sizing: border-box;  
     }  
 </style>  
